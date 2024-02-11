16e40b1931e8d404eb5346a55c1d9dea145573f7
Vladimir Shevchenko <prstud@yandex.ru>
2024-02-11 22:52:38 +0300

    Add about url path, view

--- src/apps/about/views.py
+++ src/apps/about/views.py
@@ -1,3 +1,17 @@
-# from django.shortcuts import render
+from django.views.generic import TemplateView
+from src.apps.about.models import AboutMe
 
-# Create your views here.
+
+class AboutMeView(TemplateView):
+    """About me page."""
+
+    model = AboutMe
+    template_name = "about/aboutme.html"
+    context_object_name = "about_note"
+
+    def get_context_data(self, **kwargs):
+        """Generate the data to be displayed on the page."""
+        context = super().get_context_data(**kwargs)
+        about_note = AboutMe.objects.order_by("-id").last()
+        context["about_note"] = about_note
+        return context
