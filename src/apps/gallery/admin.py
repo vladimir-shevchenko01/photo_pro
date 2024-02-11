7d368d33eb30328c3c457150f0764c2d8c68b92e
Vladimir Shevchenko <prstud@yandex.ru>
2024-02-11 22:24:23 +0300

    Add gallery admin.py

--- src/apps/gallery/admin.py
+++ src/apps/gallery/admin.py
@@ -1,3 +1,45 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from src.apps.gallery.models import Category, DetailPageImages, MainBanner
+
+
+@admin.register(MainBanner)
+class MainBannerAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "title_text",
+        "image",
+    )
+
+    list_editable = (
+        "title_text",
+        "image",
+    )
+
+    list_display_links = ("title",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "is_published",
+    )
+
+    list_editable = (
+        "title",
+        "is_published",
+    )
+
+
+@admin.register(DetailPageImages)
+class DetailPageImagesAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "is_published",
+        "category",
+        "image",
+    )
+
+    list_editable = ("is_published",)
