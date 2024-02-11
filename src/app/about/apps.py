c88185808ded20f927e49cb467dbaac5dc2f5d4d
Vladimir Shevchenko <prstud@yandex.ru>
2024-02-11 14:47:26 +0300

    Formed project structure

---
+++ src/app/about/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AboutConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "src.app.about"
+    verbose_name = "Об авторе"
