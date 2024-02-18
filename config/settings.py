d049d162afac58927cca5af25ed153f46bee0c54
Vladimir Shevchenko <prstud@yandex.ru>
2024-02-18 10:38:26 +0300

    add CSRF_TRUSTED_ORIGINS to settings and env

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
